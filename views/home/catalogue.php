2c8aadbc42fb4f3c8c47eb36026bd1f65fda483d
pierre-canet <pierre.canet@laplateforme.io>
2025-08-29 11:04:08 +0200

    création d'une page catalogue.php dans le dossier home/ et initialisation d'un début de code pour cette page

---
+++ views/home/catalogue.php
@@ -0,0 +1,79 @@
+<?php
+//navbar
+//titre
+//phrase d'intro
+//barre de recherche + filtres
+//Livres
+//grille affichage livres
+//Films
+//grille affichage films
+//Jeux vidéos
+//grille affichage jeux vidéos
+
+/*
+    *Fonctions routeur.php
+    _
+*/
+
+/*
+    *Fonctions model.php
+    _
+*/
+
+/*
+    *Fonctions view.php
+    _créer une fonction load_view_with_layout(){}
+*/
+
+/*
+    *Fonctions home_controller.php
+*/
+
+
+
+//[ [Accueil] [A propos] [Catalogue] [Profil] [Contact] [Deconnexion] ]
+//titre
+//phrase d'intro
+//[barre de recherche] [Filtres] [Rechercher]
+//Livres
+//[] [] []
+//[] [] []
+//Films
+//[] [] []
+//[] [] []
+//Jeux vidéos
+//[] [] []
+//[] [] []
+
+?>
+
+<div class="page-header">
+    <div class="container">
+        <h1><?php e($title); ?></h1>
+    </div>
+    <p>
+        Trouvez votre bonheur dans notre vaste catalogue parmi des livres, des films et même des 
+        jeux vidéos !
+    </p>
+</div>
+
+<section>
+    <form action="">
+
+    </form>
+</section>
+
+<section>
+    <h2>Livres</h2>
+    <div></div>
+</section>
+
+<section>
+    <h2>Films</h2>
+    <div></div>
+</section>
+
+<section>
+    <h2>Jeux vidéos</h2>
+    <div></div>
+</section>
